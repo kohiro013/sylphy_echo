a5eb2e4f5f0809655efe63af9220b80834e24413
kohiro <akasha013@gmail.com>
2023-05-28 11:23:10 +0900

    関数化

--- Graphics/Src/main.cpp
+++ Graphics/Src/main.cpp
@@ -8,17 +8,18 @@ namespace plt = matplotlibcpp;
 const int HALF_SECTION_SIZE = 45;	// 半区画の長さ
 const int SECTION_SIZE = 2 * HALF_SECTION_SIZE;		// 区画の長さ
 const int WALL_SIZE = 6;			// 壁の厚さ
-const int MAZE_SIZE = 16;			// 表示する区画数
+const int MAZE_SIZE = 32;			// 表示する区画数
 
-int main() {
+void plotAdditionalLine(void)
+{
 	// 区画線の表示
 	std::map<std::string, std::string> args = {
 		{"color", "silver"}, 
 		{"linestyle", "--"}
 	};
-	for (int i = 0; i <= MAZE_SIZE * 2 + 1; i++) {
+	for (int i = 0; i <= MAZE_SIZE * 2; i++) {
 		double x_min = -HALF_SECTION_SIZE;
-		double x_max = MAZE_SIZE * SECTION_SIZE;
+		double x_max = MAZE_SIZE * SECTION_SIZE - HALF_SECTION_SIZE;
 		double x_min2max = x_min + i * HALF_SECTION_SIZE;
 		double x_max2min = x_max - i * HALF_SECTION_SIZE;
 
@@ -34,7 +35,10 @@ int main() {
 			plt::plot({x_max2min, x_max}, {x_max, x_max2min}, args);
 		} else;
 	}
+}
 
+void plotPillar(void)
+{
 	// 柱の表示
 	for(int j = 0; j <= MAZE_SIZE; j++) {
 		for(int i = 0; i <= MAZE_SIZE; i++ ) {
@@ -45,15 +49,66 @@ int main() {
 			plt::fill(x, y, {{"color", "red"}});
 		}
 	}
+}
 
-	application::maze::getInstance().setDebugData();
-	for(int i = 0; i < MAZE_X; i++) {
-		for(int j = 0; j < MAZE_Y; j++) {
-			t_maze temp = application::maze::getInstance().getGlobalData(j, i);
-			printf("%02d, ", temp.byte&0x0f);
+void plotWall(int x, int y, int dir)
+{
+	double x_center;
+	double y_center;
+	std::vector<double> x_plot;
+	std::vector<double> y_plot;
+
+	if(dir == EAST || dir == WEST) {
+		y_center = y * SECTION_SIZE;
+		if(dir == WEST) {
+			x_center = x * SECTION_SIZE - HALF_SECTION_SIZE;
+		} else {
+			x_center = x * SECTION_SIZE + HALF_SECTION_SIZE;
 		}
-		printf("\r\n");
+		x_plot = {x_center - WALL_SIZE/2, x_center - WALL_SIZE/2, x_center + WALL_SIZE/2, x_center + WALL_SIZE/2};
+		y_plot = {y_center - HALF_SECTION_SIZE, y_center + HALF_SECTION_SIZE, y_center + HALF_SECTION_SIZE, y_center - HALF_SECTION_SIZE};
+		
+	} else if(dir == NORTH || dir == SOUTH) {
+		x_center = x * SECTION_SIZE;
+		if(dir == SOUTH) {
+			y_center = y * SECTION_SIZE - HALF_SECTION_SIZE;
+		} else {
+			y_center = y * SECTION_SIZE + HALF_SECTION_SIZE;
+		}
+		x_plot = {x_center - HALF_SECTION_SIZE, x_center - HALF_SECTION_SIZE, x_center + HALF_SECTION_SIZE, x_center + HALF_SECTION_SIZE};
+		y_plot = {y_center - WALL_SIZE/2, y_center + WALL_SIZE/2, y_center + WALL_SIZE/2, y_center - WALL_SIZE/2};
 	}
+	plt::fill(x_plot, y_plot, {{"color", "red"}});
+}
+
+void plotWallAll(void)
+{
+	application::maze::getInstance().setDebugData();
+
+	plotPillar();
+	for(int i = 0; i < MAZE_X; i++) {
+		plotWall(i, 0, SOUTH);
+	}
+
+	for(int j = 0; j < MAZE_Y; j++) {
+		plotWall(0, j, WEST);
+	}
+
+	for(int j = 0; j < MAZE_Y; j++) {
+		for(int i = 0; i < MAZE_X; i++) {
+			if(application::maze::getInstance().getGlobalData(i, j, NORTH) == true) {
+				plotWall(i, j, NORTH);
+			} else;
+
+			if(application::maze::getInstance().getGlobalData(i, j, EAST) == true) {
+				plotWall(i, j, EAST);
+			} else;
+		}
+	}
+}
+
+int main() {
+	plotWallAll();
 
 	plt::axis("equal");
 	plt::tight_layout();
